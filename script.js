5e2a544809a4d7561eea112935d2ad82164b54da
Nicholas <197684491+flitewidgeon@users.noreply.github.com>
2025-12-08 18:01:50 +1300

    Write the logic to play the entire game

--- script.js
+++ script.js
@@ -17,75 +17,89 @@ function getHumanChoice(){
 }
 
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
 
-function playRound(humanChoice, computerChoice){
-	humanChoice = humanChoice.toLowerCase();
-	let roundWinner;
-	if (humanChoice == 'rock'){
-		switch(computerChoice){
-		// No round winner for a draw
-		case 'rock':
-			roundWinner = 'none';
+	let humanScore = 0;
+	let computerScore = 0;
+
+	function playRound(humanChoice, computerChoice){
+		humanChoice = humanChoice.toLowerCase();
+
+		console.log(`You chose ${humanChoice} and the Computer chose ${computerChoice}`);
+
+		let roundWinner;
+
+		if (humanChoice == 'rock'){
+			switch(computerChoice){
+			// No round winner for a draw
+			case 'rock':
+				roundWinner = 'none';
+				break;
+			case 'paper':
+				roundWinner = 'computer';
+				break;
+			case 'scissors':
+				roundWinner = 'player';
+				break;
+			}
+		}
+		else if (humanChoice == 'paper'){
+			switch(computerChoice){
+			case 'rock':
+				roundWinner = 'player';
+				break;
+			case 'paper':
+				roundWinner = 'none';
+				break;
+			case 'scissors':
+				roundWinner = 'computer';
+				break;
+			}
+		}
+		else if(humanChoice == 'scissors'){
+			switch(computerChoice){
+			case 'rock':
+				roundWinner = 'computer';
+				break;
+			case 'paper':
+				roundWinner = 'player'
+				break;
+			case 'scissors':
+				roundWinner = 'none';
+				break;
+			}
+		}
+
+		let message;
+		switch(roundWinner){
+		case 'player':
+			message = `You win! ${humanChoice} beats ${computerChoice}`;
 			break;
-		case 'paper':
-			roundWinner = 'computer';
+		case 'computer':
+			message = `You lose! ${computerChoice} beats ${humanChoice}`;
 			break;
-		case 'scissors':
-			roundWinner = 'player';
-			break
+		case 'none':
+			message = `It's a draw!`;
+			break;
+		}
+		console.log(message);
+
+		// Increment human score or computer score based on round winner
+		if (roundWinner == 'player'){
+			humanScore++;
+		} 
+		if (roundWinner == 'computer'){
+			computerScore++;
 		}
 	}
-	else if (humanChoice = 'paper'){
-		switch(computerChoice){
-		case 'rock':
-			roundWinner = 'player';
-			break;
-		case 'paper':
-			roundWinner = 'none';
-			break;
-		case 'scissors':
-			roundWinner = 'computer';
-			break;
-		}
-	}
-	else if(humanChoice = 'scissors'){
-		switch(computerChoice){
-		case 'rock':
-			roundWinner = 'computer';
-			break;
-		case 'paper':
-			roundWinner = 'player'
-			break;
-		case 'scissors':
-			roundWinner = 'none';
-			break;
-		}
-	}
 
-	let message;
-	switch(roundWinner){
-	case 'player':
-		message = `You win! ${humanChoice} beats ${computerChoice}`;
-		break;
-	case 'computer':
-		message = `You lose! ${computerChoice} beats ${humanChoice}`;
-	case 'none':
-		message = `It's a draw!`;
-	}
-	console.log(message);
+	for (let i = 0; i < 5; i++){
+		const humanSelection = getHumanChoice();
+		const computerSelection = getComputerChoice();
 
-	// Increment human score or computer score based on round winner
-	if (roundWinner == 'player'){
-		humanScore++;
-	} else if (roundWinner == 'computer'){
-		computerScore++;
+		playRound(humanSelection, computerSelection);
+		console.log(`Player Score: ${humanScore} | Computer Score ${computerScore}`);
 	}
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-console.log(`Player Score: ${humanScore} | Computer Score ${computerScore}`);
+playGame();
