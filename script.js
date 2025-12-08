7ed82e43cd899c8eaa419cdeeacd40843633a6e6
Nicholas <197684491+flitewidgeon@users.noreply.github.com>
2025-12-08 17:47:38 +1300

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -19,3 +19,73 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+	humanChoice = humanChoice.toLowerCase();
+	let roundWinner;
+	if (humanChoice == 'rock'){
+		switch(computerChoice){
+		// No round winner for a draw
+		case 'rock':
+			roundWinner = 'none';
+			break;
+		case 'paper':
+			roundWinner = 'computer';
+			break;
+		case 'scissors':
+			roundWinner = 'player';
+			break
+		}
+	}
+	else if (humanChoice = 'paper'){
+		switch(computerChoice){
+		case 'rock':
+			roundWinner = 'player';
+			break;
+		case 'paper':
+			roundWinner = 'none';
+			break;
+		case 'scissors':
+			roundWinner = 'computer';
+			break;
+		}
+	}
+	else if(humanChoice = 'scissors'){
+		switch(computerChoice){
+		case 'rock':
+			roundWinner = 'computer';
+			break;
+		case 'paper':
+			roundWinner = 'player'
+			break;
+		case 'scissors':
+			roundWinner = 'none';
+			break;
+		}
+	}
+
+	let message;
+	switch(roundWinner){
+	case 'player':
+		message = `You win! ${humanChoice} beats ${computerChoice}`;
+		break;
+	case 'computer':
+		message = `You lose! ${computerChoice} beats ${humanChoice}`;
+	case 'none':
+		message = `It's a draw!`;
+	}
+	console.log(message);
+
+	// Increment human score or computer score based on round winner
+	if (roundWinner == 'player'){
+		humanScore++;
+	} else if (roundWinner == 'computer'){
+		computerScore++;
+	}
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(`Player Score: ${humanScore} | Computer Score ${computerScore}`);
