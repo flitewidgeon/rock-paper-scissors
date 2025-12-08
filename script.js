435b20f69584948028feaed163266028dba7ec9b
Nicholas <197684491+flitewidgeon@users.noreply.github.com>
2025-12-08 17:19:38 +1300

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+function getComputerChoice(){
+	// get a random number: 0, 1, or 2
+	const randomNum = Math.floor(Math.random()* 3);
+	let choice;
+	if (randomNum == 0){
+		choice = 'rock';
+	} else if (randomNum == 1){
+		choice = 'paper';
+	} else {
+		choice = 'scissors';
+	}
+	return choice;
+}
